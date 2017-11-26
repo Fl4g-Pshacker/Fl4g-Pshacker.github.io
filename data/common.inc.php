72989a26d4f200a2bebf33edb55b7c33b0ee1d88
Fl4g-Pshacker <33883479+Fl4g-Pshacker@users.noreply.github.com>
2017-11-26 17:07:17 +0800

    Add files via upload

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'by004';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = 'C-dsdD_!123(o_0)9#!^.PX';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
